e57f18b078e4318b57c9a932482ea9677a46d6be
Rob Hoelz <rob@hoelz.ro>
2014-09-11 23:04:04 -0500

    Start on Test::Meta

---
+++ lib/Test/Meta.pm
@@ -0,0 +1,10 @@
+class Test::Meta {
+    submethod BUILD(:$code) {
+        # XXX run the code
+    }
+}
+
+macro test($block) is export {
+    my $code = $block.Str;
+    return quasi { Test::Meta.new(:$code) }
+}
