75fcc96b5826774b19d4c4fa7a0f2b1d376f59d8
Rob Hoelz <rob@hoelz.ro>
2014-09-13 10:27:41 -0500

    Flesh out basics of BUILD

--- lib/Test/Meta.pm
+++ lib/Test/Meta.pm
@@ -1,6 +1,41 @@
+use v6;
+use File::Temp;
+use TAP::Parser;
+use Test;
+
 class Test::Meta {
+    has $!tap-result;
+
     submethod BUILD(:$code) {
-        # XXX run the code
+        my ( $input, $input_h ) = tempfile(:!unlink);
+        my ( $output, $output_h ) = tempfile;
+
+        $input_h.say($code);
+
+        .close for $input_h, $output_h;
+
+        # XXX better invocation
+        # XXX use $?COMPILER once we have it
+        my $status = run('sh', '-c', "perl6 $input > $output");
+        unless $status {
+            die "perl6 $input died with status $status";
+        }
+
+        my $source     = TAP::Parser::Async::Source::File.new(filename => $output);
+        my $tap-parser = $source.make-parser;
+
+        await $tap-parser;
+        $!tap-result = $tap-parser.result;
+    }
+
+    method ok {
+        pass;
+        self
+    }
+
+    method name($expected) {
+        pass;
+        self
     }
 }
 
